076e0339eb9de5edacc88b3b84a59be8cdab4387
Oded Regev <odedre@gmail.com>
2016-03-16 10:24:29 +0200

    ignore HTML-Documentation for now in the release

--- ios_release.sh
+++ ios_release.sh
@@ -8,24 +8,24 @@ xcodebuild clean -target OutbrainSDK
 xcodebuild -target OBFramework
 cd ..
 
-doxygen doxygen.xml
+# doxygen doxygen.xml
 
 rm -fr OBSDK-Release
 mkdir OBSDK-Release
 mkdir OBSDK-Release/SDK/
 mkdir OBSDK-Release/Samples/
-mkdir OBSDK-Release/HTML-Documentation/
+# mkdir OBSDK-Release/HTML-Documentation/
 
 
 cp -a OutbrainSDK.framework OBSDK-Release/SDK/
 cp -a OutbrainSDK.framework OBSDK-Release/Samples/
 cp -fa Samples/ OBSDK-Release/Samples/
-cp -rf HTML-Documentation/ OBSDK-Release/HTML-Documentation/
+# cp -rf HTML-Documentation/ OBSDK-Release/HTML-Documentation/
 cp -rf README.md OBSDK-Release/
 cp -rf Release-Notes.txt OBSDK-Release/
 
 # clean up the folder
-rm -fr HTML-Documentation/
+# rm -fr HTML-Documentation/
 
 rm -fr ~/Desktop/Release/iOS
 mkdir -p ~/Desktop/Release/iOS
