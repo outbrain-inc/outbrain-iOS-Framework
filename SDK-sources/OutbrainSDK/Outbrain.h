37d959f0894c7ae3cfe3c52d188dbb2fea9dea37
Oded Regev <odedre@gmail.com>
2017-11-20 10:44:47 +0200

    remove redundent code

--- SDK-sources/OutbrainSDK/Outbrain.h
+++ SDK-sources/OutbrainSDK/Outbrain.h
@@ -20,8 +20,6 @@
 // The current version of the sdk
 extern NSString * const OB_SDK_VERSION;
 
-extern NSString *const kIS_COOKIES_ENABLE_KEY;
-
 #define SYSTEM_VERSION_LESS_THAN(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 
 /**
