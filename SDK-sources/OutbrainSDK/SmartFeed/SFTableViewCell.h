36966f048f3d45d65a917dea0c8f4a387cc519aa
Oded Regev <odedre@gmail.com>
2018-05-03 18:13:24 +0300

    add *Sponsored* to AppOBTableViewCell

--- SDK-sources/OutbrainSDK/SmartFeed/SFTableViewCell.h
+++ SDK-sources/OutbrainSDK/SmartFeed/SFTableViewCell.h
@@ -13,6 +13,7 @@
 @property (nonatomic, weak) IBOutlet UIImageView *recImageView;
 @property (nonatomic, weak) IBOutlet UILabel *recTitleLabel;
 @property (nonatomic, weak) IBOutlet UILabel *recSourceLabel;
+@property (nonatomic, weak) IBOutlet UILabel *sponsoredLabel;
 
 
 @end
