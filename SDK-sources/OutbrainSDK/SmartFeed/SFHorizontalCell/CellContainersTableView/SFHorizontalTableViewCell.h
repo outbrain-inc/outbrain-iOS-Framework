ffe36675483796e29a318070458de7b724af5bc2
Oded Regev <odedre@gmail.com>
2018-07-18 12:40:07 +0300

    refactor files structure and support grid with 2 items in table view

--- SDK-sources/OutbrainSDK/SmartFeed/SFHorizontalCell/CellContainersTableView/SFHorizontalTableViewCell.h
+++ SDK-sources/OutbrainSDK/SmartFeed/SFHorizontalCell/CellContainersTableView/SFHorizontalTableViewCell.h
@@ -12,7 +12,7 @@
 @interface SFHorizontalTableViewCell : UITableViewCell
 
 @property (nonatomic, weak) IBOutlet SFHorizontalView *horizontalView;
-@property (nonatomic, weak) IBOutlet UILabel *moreFromLabel;
-@property (nonatomic, weak) IBOutlet UIImageView *moreFromImageView;
+@property (nonatomic, weak) IBOutlet UILabel *titleLabel;
+@property (nonatomic, weak) IBOutlet UIImageView *publisherImageView;
 
 @end
--- SDK-sources/OutbrainSDK/SmartFeed/SFHorizontalCell/SFHorizontalView/SFHorizontalView.h
+++ SDK-sources/OutbrainSDK/SmartFeed/SFHorizontalCell/SFHorizontalView/SFHorizontalView.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <OutbrainSDK/OutbrainSDK.h>
 
-@interface SFHorizontalView : UIView 
+@interface SFHorizontalView : UIView <UICollectionViewDataSource, UICollectionViewDelegate>
 
 typedef void(^OnClick)(OBRecommendation *rec);
 
