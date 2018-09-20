76a2a13fb091e2effdc51fce17e7ad1072406784
Oded Regev <odedre@gmail.com>
2018-09-20 11:42:42 +0300

    check if video included in response

--- SDK-sources/OutbrainSDK/SmartFeed/SmartFeedManager/SmartFeedManager.m
+++ SDK-sources/OutbrainSDK/SmartFeed/SmartFeedManager/SmartFeedManager.m
@@ -221,8 +221,7 @@
         [[SFImageLoader sharedInstance] loadImageToCacheIfNeeded:rec.image.url];
     }
     
-    BOOL videoIncluded = response.settings.isSmartFeed; // TODO check in response if video is included
-    if (videoIncluded) {
+    if ([self isVideoIncludedInResponse:response]) {
         NSURL *videoURL = [NSURL URLWithString:@"https://static-test.outbrain.com/video/app/vidgetInApp.html?platform=ios&widgetId=AR_1&publisherId=111&sourceId=222"];
         SFItemData *item = [[SFItemData alloc] initWithVideoUrl:videoURL widgetId:response.request.widgetId];
         [self.smartFeedItemsArray addObject:item];
