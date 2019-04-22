97174d149378aa3f250923735f73d87550ee7bf8
alonshp <alonshp1@gmail.com>
2019-04-22 14:50:12 +0300

    default implementation for SmartfeedListener in the SDK

---
+++ SDK-sources/OutbrainSDK/SmartFeed/SmartFeedManager/SFDefaultDelegate.h
@@ -0,0 +1,14 @@
+//
+//  SFDefaultDelegate.h
+//  OutbrainSDK
+//
+//  Created by Alon Shprung on 4/22/19.
+//  Copyright © 2019 Outbrain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SmartFeedManager.h"
+
+@interface SFDefaultDelegate : NSObject <SmartFeedDelegate>
+
+@end
