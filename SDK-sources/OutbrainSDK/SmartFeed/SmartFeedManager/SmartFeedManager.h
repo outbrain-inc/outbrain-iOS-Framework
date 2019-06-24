ce651a16843b8152c989683e27eaf5e8310828ea
Oded Regev <odedre@gmail.com>
2019-06-24 14:37:45 +0300

    add documentation to SmartFeedDelegate

--- SDK-sources/OutbrainSDK/SmartFeed/SmartFeedManager/SmartFeedManager.h
+++ SDK-sources/OutbrainSDK/SmartFeed/SmartFeedManager/SmartFeedManager.h
@@ -14,22 +14,68 @@
 
 @protocol SmartFeedDelegate <NSObject>
 
+/**
+ * @brief called when the user has tapped on a recommendation in the Smartfeed
+ *
+ * @param rec - the OBRecommendation the user has tapped on
+ **/
 -(void) userTappedOnRecommendation:(OBRecommendation *_Nonnull)rec;
 
+/**
+ * @brief called when the user has tapped on the "ad choice" icon.
+ *
+ * @param url - the url to open in an external browser to support "Ad choices" guidelines
+ **/
 -(void) userTappedOnAdChoicesIcon:(NSURL *_Nonnull)url;
 
+/**
+ * @brief called when the user has tapped on the video player in the Smartfeed
+ *
+ * @param url - the video "click url" to open in an external browser
+ **/
 -(void) userTappedOnVideoRec:(NSURL *_Nonnull)url;
 
+/**
+ * @brief called when the user has tapped on the Outbrain logo. Please make sure to follow the guidelines.
+ *
+ **/
 -(void) userTappedOnOutbrainLabeling;
 
 @optional
 -(void) smartFeedResponseReceived:(NSArray<OBRecommendation *> *_Nonnull)recommendations forWidgetId:(NSString *_Nonnull)widgetId;
 
+/**
+ * @brief app developer implement this method to let the SmartFeedManager check if there is a video currently playing
+ * in the app (in an article for example). If the app returns "true" the SDK will not play a video in the Smartfeed.
+ *
+ * @return true if video is currently playing in the app
+ **/
 -(BOOL) isVideoCurrentlyPlaying;
 
+/**
+ * @brief belongs to "custom UI" - app developer can implement this method to customize the UI of Smartfeed carousel horizontal item.
+ *
+ * @return the CGSize of Smartfeed horizontal carousel item.
+ **/
 -(CGSize) carouselItemSize;
 
+/**
+ * @brief belongs to "custom UI" - app developer can implement this method to manually configure an "horizontal cell" before it
+ * is displayed to the user.
+ *
+ * @param sfCollectionViewCell - the SFCollectionViewCell to configure
+ * @param rec - the content of the recommendation to be displayed in the cell
+ *
+ **/
 -(void) configureHorizontalItem:(SFCollectionViewCell * _Nonnull)sfCollectionViewCell withRec:(OBRecommendation * _Nonnull)rec;
+
+/**
+ * @brief this method is relevant only in case Smartfeed is displayed in the middle of the feed (not at the end).
+ * SmartFeedManager will notify the app developer when the Smartfeed is ready with recs to be displayed so the
+ * data source can reload the feed to integrate Outbrain recs within it.
+ *
+ **/
+-(void) smartfeedIsReadyWithRecs;
 
 @end
 
