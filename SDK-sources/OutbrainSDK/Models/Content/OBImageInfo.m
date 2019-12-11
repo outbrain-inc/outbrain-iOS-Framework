e5aeb1f12f976ffedeae092140db5d173447b703
Oded Regev <odedre@gmail.com>
2019-12-11 10:28:07 +0200

    rename OBImage to OBImageInfo, part 2

--- SDK-sources/OutbrainSDK/Models/Content/OBImageInfo.m
+++ SDK-sources/OutbrainSDK/Models/Content/OBImageInfo.m
@@ -1,16 +1,16 @@
 //
-//  OBImage.m
+//  OBImageInfo.m
 //  OutbrainSDK
 //
 //  Created by Oded Regev on 12/12/13.
 //  Copyright (c) 2013 Outbrain inc. All rights reserved.
 //
 
-#import "OBImage.h"
+#import "OBImageInfo.h"
 #import "OBContent_Private.h"
 
 
-@implementation OBImage
+@implementation OBImageInfo
 
 + (NSArray *)requiredKeys
 {
@@ -20,42 +20,20 @@
 + (NSDictionary *)propertiesMap
 {
     return @{
-             @"width":@"width",
-             @"height":@"height",
-             @"url":@"url"
+             @"width":  @"width",
+             @"height": @"height",
+             @"url":    @"url"
              };
 }
 
 + (Class)propertyClassForKey:(NSString *)key
 {
-    if([key isEqualToString:@"url"]) return [NSURL class];
+    if ([key isEqualToString:@"url"]) {
+        return [NSURL class];
+    }
+    
     return [super propertyClassForKey:key];
 }
 
-#pragma mark - Getters & Setters
-
-- (NSURL *)url {
-    return url;
-}
-
-- (CGFloat)width {
-    return width;
-}
-
-- (CGFloat)height {
-    return height;
-}
-
-- (void)setUrl:(NSURL *)aUrl {
-    url = aUrl;
-}
-
-- (void)setHeight:(CGFloat)aHeight {
-    height = aHeight;
-}
-
-- (void)setWidth:(CGFloat)aWidth {
-    width = aWidth;
-}
 
 @end
