10f3e0e4591c88948ae6e801951f8ec083ac34a5
Oded Regev <odedre@gmail.com>
2020-01-15 14:16:34 +0200

    remove comment

--- SDK-sources/OutbrainSDK/SmartFeed/SmartFeedManager/SmartFeedManager.m
+++ SDK-sources/OutbrainSDK/SmartFeed/SmartFeedManager/SmartFeedManager.m
@@ -367,7 +367,7 @@ NSString * const kCustomUIIdentifier = @"CustomUIIdentifier";
 
 -(NSArray *) createCarouselItemArrayFromResponse:(OBRecommendationResponse *)response templateType:(SFItemType)templateType widgetTitle:(NSString *)widgetTitle {
     NSArray *recommendations = response.recommendations;
-    //if (response.settings.isTrendingInCategoryCard && recommendations.count > 0) {
+
     if (response.settings.isTrendingInCategoryCard) {
         OBRecommendation *rec = recommendations[0];
         response.settings.widgetHeaderText = [NSString stringWithFormat:@"%@ %@", response.settings.widgetHeaderText, rec.categoryName];
