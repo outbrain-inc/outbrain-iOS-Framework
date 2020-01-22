3100b625c4ec9055e58bb20eb851c406a9afc6eb
Oded Regev <odedre@gmail.com>
2020-01-22 11:48:59 +0200

    add dev hack for advertising id in test mode

--- SDK-sources/OutbrainSDK/AdIds/OBAppleAdIdUtil.m
+++ SDK-sources/OutbrainSDK/AdIds/OBAppleAdIdUtil.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2014 Outbrain. All rights reserved.
 //
 
+#import "OBUtils.h"
+#import "OutbrainManager.h"
 #import "OBAppleAdIdUtil.h"
 #import <AdSupport/AdSupport.h>
 
@@ -19,6 +21,11 @@
 }
 
 + (NSString *)getAdvertiserId {
+    if ([OutbrainManager sharedInstance].testMode &&
+        [[OBUtils deviceModel] isEqualToString:@"Simulator"])
+    {
+        return @"F22700D5-1D49-42CC-A183-F3676526035F"; // dev hack because simulator returns 0000-0000-0000-0000
+    }
     return [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
 }
 
