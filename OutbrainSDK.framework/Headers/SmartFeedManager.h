f3b00e8fe93a6992916d5db1cde28019dc6f3e8f
Oded Regev <odedre@gmail.com>
2020-04-20 14:44:08 +0300

    outbrain sdk v3.8.6

--- OutbrainSDK.framework/Headers/SmartFeedManager.h
+++ OutbrainSDK.framework/Headers/SmartFeedManager.h
@@ -105,6 +105,7 @@ typedef enum
 
 @property (nonatomic, assign) NSInteger outbrainWidgetIndex;
 @property (nonatomic, assign) BOOL isInMiddleOfScreen;
+@property (nonatomic, assign) BOOL darkMode;
 @property (nonatomic, assign) BOOL disableCellShadows;
 @property (nonatomic, assign) BOOL useDefaultCollectionViewDelegate;
 @property (nonatomic, assign, readonly) BOOL hasMore;
