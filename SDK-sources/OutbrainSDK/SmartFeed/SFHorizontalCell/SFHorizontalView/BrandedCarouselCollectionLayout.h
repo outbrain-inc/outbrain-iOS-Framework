37b21df6839133c2df3901c4bed110deb97bebc1
Oded Regev <odedre@gmail.com>
2020-06-18 14:51:48 +0300

    BrandedCarouselCollectionLayout impl

--- SDK-sources/OutbrainSDK/SmartFeed/SFHorizontalCell/SFHorizontalView/BrandedCarouselCollectionLayout.h
+++ SDK-sources/OutbrainSDK/SmartFeed/SFHorizontalCell/SFHorizontalView/BrandedCarouselCollectionLayout.h
@@ -8,10 +8,6 @@
 
 #import "PageCollectionLayout.h"
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface BrandedCarouselCollectionLayout : PageCollectionLayout
 
 @end
-
-NS_ASSUME_NONNULL_END
--- SDK-sources/OutbrainSDK/SmartFeed/SFHorizontalCell/SFHorizontalView/BrandedCarouselCollectionLayout.m
+++ SDK-sources/OutbrainSDK/SmartFeed/SFHorizontalCell/SFHorizontalView/BrandedCarouselCollectionLayout.m
@@ -10,4 +10,36 @@
 
 @implementation BrandedCarouselCollectionLayout
 
+// Branded Carousel
+- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity
+{
+    CGSize collectionViewSize = self.collectionView.bounds.size;
+    CGFloat width = collectionViewSize.width;
+    CGFloat halfWidth = width * 0.5;
+
+    CGFloat direction = (proposedContentOffset.x > self.collectionView.contentOffset.x ? 1 : 0);
+    CGFloat pageOffsetX = self.itemSize.width * floor(self.collectionView.contentOffset.x / self.itemSize.width);
+    CGFloat proposedContentOffsetCenterX = pageOffsetX + (width * direction);
+    CGRect proposedRect = CGRectMake(proposedContentOffsetCenterX, 0, collectionViewSize.width, collectionViewSize.height);
+
+    UICollectionViewLayoutAttributes *candidateAttributes;
+
+    for (UICollectionViewLayoutAttributes *attributes in [self layoutAttributesForElementsInRect:proposedRect]) {
+        if (attributes.representedElementCategory != UICollectionElementCategoryCell) continue;
+
+        candidateAttributes = attributes;
+        if (direction == 1) {
+            break; // if direction right, take the first item (break), else take the last
+        }
+    }
+    
+    if (direction == 1)
+        proposedContentOffset.x = candidateAttributes.center.x - halfWidth;
+    else {
+        proposedContentOffset.x = candidateAttributes.center.x - halfWidth - self.itemSize.width;
+    }
+
+    return proposedContentOffset;
+}
+
 @end
