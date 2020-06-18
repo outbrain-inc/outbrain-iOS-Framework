d1439654b579e1ffdbf6585188214052b4c6f05a
Oded Regev <odedre@gmail.com>
2020-06-18 14:49:05 +0300

    add BrandedCarouselCollectionLayout files

---
+++ SDK-sources/OutbrainSDK/SmartFeed/SFHorizontalCell/SFHorizontalView/BrandedCarouselCollectionLayout.h
@@ -0,0 +1,17 @@
+//
+//  BrandedCarouselCollectionLayout.h
+//  OutbrainSDK
+//
+//  Created by oded regev on 18/06/2020.
+//  Copyright © 2020 Outbrain. All rights reserved.
+//
+
+#import "PageCollectionLayout.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BrandedCarouselCollectionLayout : PageCollectionLayout
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SDK-sources/OutbrainSDK/SmartFeed/SFHorizontalCell/SFHorizontalView/BrandedCarouselCollectionLayout.m
@@ -0,0 +1,13 @@
+//
+//  BrandedCarouselCollectionLayout.m
+//  OutbrainSDK
+//
+//  Created by oded regev on 18/06/2020.
+//  Copyright © 2020 Outbrain. All rights reserved.
+//
+
+#import "BrandedCarouselCollectionLayout.h"
+
+@implementation BrandedCarouselCollectionLayout
+
+@end
