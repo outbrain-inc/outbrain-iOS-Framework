6bef7a9f3e3d94c1657f7c34138ee128203d9256
Oded Regev <odedre@gmail.com>
2020-07-27 13:56:18 +0300

    add new class OBPlatformRequest for platforms

---
+++ SDK-sources/OutbrainSDK/Models/Requests/OBPlatformRequest.h
@@ -0,0 +1,17 @@
+//
+//  OBPlatformRequest.h
+//  OutbrainSDK
+//
+//  Created by oded regev on 27/07/2020.
+//  Copyright © 2020 Outbrain. All rights reserved.
+//
+
+#import <OutbrainSDK/OutbrainSDK.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OBPlatformRequest : OBRequest
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SDK-sources/OutbrainSDK/Models/Requests/OBPlatformRequest.m
@@ -0,0 +1,13 @@
+//
+//  OBPlatformRequest.m
+//  OutbrainSDK
+//
+//  Created by oded regev on 27/07/2020.
+//  Copyright © 2020 Outbrain. All rights reserved.
+//
+
+#import "OBPlatformRequest.h"
+
+@implementation OBPlatformRequest
+
+@end
