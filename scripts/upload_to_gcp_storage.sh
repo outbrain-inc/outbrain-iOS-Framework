de7c518c19a229dce811d3b54fe5bfd5bb6d4d6b
Oded Regev <oded@cherrypick-consulting.com>
2021-04-08 11:26:40 +0300

    script to upload artificat to GCP

--- scripts/upload_to_gcp_storage.sh
+++ scripts/upload_to_gcp_storage.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-GCP_API_KEY=$GCP_API_KEY
+
 
 if [[ $# != 3 ]]; then
-echo "parameters are missing"
-exit 1
+	echo "parameters are missing"
+	echo "./upload_to_gcp_storage.sh FROM_PATH  FILE_NAME  NEW_SDK_VER"
+	exit 1
 fi
 
 FROM_PATH=$1
@@ -20,17 +21,15 @@ echo "*************************************************************************"
 echo ""
 
 
-ACCESS_TOKEN="ya29.c.Kp8B-wcVNys3hQbtgDNAP1a7t1fOIqUGrRiGhc0OPFKG2nqe_1OtZjXopOi81ybbG66Q9rizj4xyZDf0pg27r8C4n_myvibYfpCaXybPF_N0dZvB60Td64nZX9r9wUB-J3hTTU9Lzcjem3Z_niYNQiNzSM6YD2l54BOcP73Sx6KCVVxjGqy8nAqN_JeIPqmKcB9zKQtK_Sd3wsU-CisfTk8u"
-echo "ACCESS_TOKEN: $ACCESS_TOKEN"
-
+echo "Request Access Token From GCP..."
 touch sdk-assets-33ff7d65d16b.json
 echo $GCP_API_KEY > sdk-assets-33ff7d65d16b.json
-ACCESS_TOKEN_2=`./get-access-token.sh sdk-assets-33ff7d65d16b.json "https://www.googleapis.com/auth/devstorage.read_write"
+ACCESS_TOKEN=`./get-access-token.sh sdk-assets-33ff7d65d16b.json "https://www.googleapis.com/auth/devstorage.read_write"`
 
-echo "ACCESS_TOKEN_2: $ACCESS_TOKEN_2"
+echo "Upload file to GCP with Access Token"
 
-# curl -v --upload-file $FROM_PATH \
-	 # -H "Authorization: Bearer $ACCESS_TOKEN" 'https://storage.googleapis.com/outbrain-sdk/$NEW_SDK_VER/$FILE_NAME'
+curl -v --upload-file $FROM_PATH \
+	 -H "Authorization: Bearer $ACCESS_TOKEN" "https://storage.googleapis.com/outbrain-sdk/$NEW_SDK_VER/$FILE_NAME"
 
 
 
