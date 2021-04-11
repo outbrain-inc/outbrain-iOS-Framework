c768f92285aba575196c3a8ce35fd22a98405002
Oded Regev <oded@cherrypick-consulting.com>
2021-04-11 14:11:16 +0300

    update .circleci/config.yml --> git push new sdk version to public repo

--- scripts/bump_sdk_version.sh
+++ scripts/bump_sdk_version.sh
@@ -22,7 +22,7 @@ git add Samples/OutbrainDemo/SmartFeed/Resources/SmartFeed-Info.plist
 
 git config credential.helper 'cache --timeout=120'
 git config --global user.email "oregev@outbrain.com"
-git config --global user.name "Alon Shprung via CircleCI"
+git config --global user.name "Oded Regev via CircleCI"
 git commit -m "[skip ci] Update SDK version to ${RELEASE_VERSION}"
 git tag "release_${RELEASE_VERSION}"
 git push origin $CIRCLE_BRANCH
