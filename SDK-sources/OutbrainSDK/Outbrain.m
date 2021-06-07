2d85e136bcb976a9065d85483da334c1676fec2f
Oded Regev via CircleCI <oregev@outbrain.com>
2021-06-07 11:22:30 +0000

    [skip ci] Update SDK version to 4.5.1

--- SDK-sources/OutbrainSDK/Outbrain.m
+++ SDK-sources/OutbrainSDK/Outbrain.m
@@ -20,7 +20,7 @@
 
 
 // The version of the sdk
-NSString * const OB_SDK_VERSION     =   @"4.5.0";
+NSString * const OB_SDK_VERSION     =   @"4.5.1";
 
 NSString * const OB_AD_NETWORK_ID   =   @"97r2b46745.skadnetwork";
 
