3c6fc2f303df5c587e7a7f2768f4f70cc31b1d3a
Oded Regev via CircleCI <oregev@outbrain.com>
2021-06-21 13:09:30 +0300

    add new target SFWebView-Dev

---
+++ Samples/OutbrainDemo/SFWebView-Dev/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  SFWebView-Dev
+//
+//  Created by Oded Regev on 21/06/2021.
+//  Copyright © 2021 Outbrain inc. All rights reserved.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
---
+++ Samples/OutbrainDemo/SFWebView-Dev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SFWebView-Dev
+//
+//  Created by Oded Regev on 21/06/2021.
+//  Copyright © 2021 Outbrain inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
