a7dbcf9a246cead507ccbc9b47c155ff6ea3209b
Oded Regev via CircleCI <oregev@outbrain.com>
2021-07-05 13:54:08 +0300

    Allow widget_id and url params to be customized by the user (shake gesture). For GTO team

--- Samples/OutbrainDemo/SFWebView-Dev/ViewController.swift
+++ Samples/OutbrainDemo/SFWebView-Dev/ViewController.swift
@@ -20,9 +20,78 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "SFWebView Demo App"
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController {
+    // Enable detection of shake motion
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            print("Why are you shaking me?")
+            self.showAlert()
+        }
+    }
+    
+    func showAlert() {
+        //1. Create the alert controller.
+        let alert = UIAlertController(title: "Edit Fields", message: "Please edit the fields below", preferredStyle: .alert)
+        
+        //2. Add the text field. You can configure it however you need.
+        alert.addTextField { (textField) in
+            textField.placeholder = "Widget ID"
+        }
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Page URL"
+        }
+        
+        // 3. Grab the value from the text field, and print it when the user clicks OK.
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+            if let widgetID = alert?.textFields?[0].text, var urlString = alert!.textFields?[1].text {
+                print("widgetID: \(widgetID)")
+                print("url: \(urlString)")
+                
+                OBConf.widgetID = widgetID;
+                
+                if (urlString.starts(with: "www")) {
+                    urlString = "https://" + urlString
+                }
+                if (self.verifyUrl(urlString: urlString)) {
+                    OBConf.baseURL = urlString
+                }
+                else {
+                    let errorAlert = UIAlertController(title: "Error", message: "URL is not valid (\(urlString))", preferredStyle: .alert)
+                    errorAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                    self.present(errorAlert, animated: true, completion: nil)
+                    return
+                }
+                
+                let successAlert = UIAlertController(title: "Success", message: "Smartfeed will load with new fields (\(widgetID)) (\(urlString))", preferredStyle: .alert)
+                successAlert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.present(successAlert, animated: true, completion: {
+                    
+                })
+            }
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        // 4. Present the alert.
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func verifyUrl (urlString: String?) -> Bool {
+        //Check for nil
+        if let urlString = urlString {
+            // create NSURL instance
+            if let url = URL(string: urlString) {
+                // check if your application can open the NSURL instance
+                return UIApplication.shared.canOpenURL(url)
+            }
+        }
+        return false
+    }
 }
 
