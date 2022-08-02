e0741bb579ed8deb6e671e2a4877312f2ebd9837
Oded Regev <oded@cherrypick-consulting.com>
2022-08-02 13:46:08 +0300

    basic example of SDK Bridge works in Swift UI (still need to adjust height and advanced delegate methods

--- Samples/OutbrainDemo/SwiftUI-Bridge/ContentView.swift
+++ Samples/OutbrainDemo/SwiftUI-Bridge/ContentView.swift
@@ -7,36 +7,114 @@
 //
 
 import SwiftUI
+import OutbrainSDK
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+
+    }
+}
+
+// Our CustomSFWidgetDelegate implementation class that expects to find a SFWidgetObservable object
+// in the environment, and set if needed.
+class MyCustomSFWidgetDelegate : NSObject {
+    var sfWidgetObservable: SFWidgetObservable?
+}
+
+extension MyCustomSFWidgetDelegate : SFWidgetDelegate {
+    func onRecClick(_ url: URL) {
+        if let sfWidgetObservable = self.sfWidgetObservable {
+            sfWidgetObservable.url = url
+            sfWidgetObservable.showSafari = true
+        }
+    }
+    
+    func didChangeHeight() {
+        //self.sfWidgetObservable.widgetHeight = 2000
+    }
+}
+
+// Our observable object class
+class SFWidgetObservable: ObservableObject {
+    @Published var showSafari:Bool = false
+    @Published var url:URL?
+    @Published var widgetHeight:CGFloat?
+}
+
+struct SFWidgetWrapper: UIViewRepresentable {
+    @EnvironmentObject var sfWidgetObservable: SFWidgetObservable
+    
+    let widgetId:String
+    let baseURL:String
+    let installationKey:String
+    let myCustomDelegate = MyCustomSFWidgetDelegate()
+    
+    func updateUIView(_ uiView: SFWidget, context: Context) {
+        uiView.configure(with: myCustomDelegate, url: baseURL, widgetId: widgetId, installationKey: installationKey)
+        myCustomDelegate.sfWidgetObservable = sfWidgetObservable
+    }
+    
+    func makeUIView(context: Context) -> SFWidget {
+        SFWidget()
+    }
+}
 
 struct ContentView: View {
+    @StateObject var sfWidgetObservable = SFWidgetObservable()
+
+    
+    let widgetId = "MB_1"
+    let baseURL = "https://mobile-demo.outbrain.com"
+    let installationKey = "NANOWDGT01"
+    
+    init() {
+        Outbrain.initializeOutbrain(withPartnerKey: "iOSSampleApp2014")
+        
+    }
     
     var body: some View {
-        ScrollView(.vertical) {
-            VStack(spacing:0) {
-                Image("article_image", bundle: Bundle.main)
-                    .resizable()
-                    .aspectRatio(16/9, contentMode: .fill)
-                Text("The Guardian")
-                    .padding()
-                    .font(.headline)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .frame(height: 80.0)
-                    .background(.blue)
-                    .foregroundColor(.white)
-                
-                Text("Suarez: Messi Was Born Great, Ronaldo Made Himself Great")
-                    .font(.system(size: 24))
-                    .fontWeight(.medium)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(EdgeInsets(top: 20, leading: 5, bottom: 20, trailing: 0    ))
-                
-                ArticleBody()
-                ArticleBody()
-                ArticleBody()
-                
-                Spacer()
+            ScrollView(.vertical) {
+                VStack(spacing:0) {
+                    Image("article_image", bundle: Bundle.main)
+                        .resizable()
+                        .aspectRatio(16/9, contentMode: .fill)
+                    Text("The Guardian")
+                        .padding()
+                        .font(.headline)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .frame(height: 80.0)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                    
+                    Text("Suarez: Messi Was Born Great, Ronaldo Made Himself Great")
+                        .font(.system(size: 24))
+                        .fontWeight(.medium)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(EdgeInsets(top: 20, leading: 5, bottom: 20, trailing: 0    ))
+                    
+                    ArticleBody()
+                    ArticleBody()
+                    ArticleBody()
+                    
+                    SFWidgetWrapper(widgetId: widgetId, baseURL: baseURL, installationKey: installationKey)
+                        .frame(height: 1000)
+                        .padding(EdgeInsets(top: 20, leading: 10, bottom: 20, trailing: 10))
+                    
+                    
+                    Spacer()
+                }
             }
-        }
+            .fullScreenCover(isPresented: $sfWidgetObservable.showSafari) {
+                SafariView(url: sfWidgetObservable.url!)
+            }
+            .environmentObject(sfWidgetObservable)
     }
 }
 
