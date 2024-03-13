77aca57bd60ad4b9c3db448fca761c292ffb0cb6
Oded Regev via CircleCI <oregev@outbrain.com>
2024-03-13 09:55:15 +0000

    update OutbrainSDK.xcframework to version 5.0.2-alpha

--- OutbrainSDK.xcframework/ios-arm64/OutbrainSDK.framework/Headers/OutbrainSDK.h
+++ OutbrainSDK.xcframework/ios-arm64/OutbrainSDK.framework/Headers/OutbrainSDK.h
@@ -2,54 +2,17 @@
 //  OutbrainSDK.h
 //  OutbrainSDK
 //
-//  Created by oded regev on 15/04/2018.
-//  Copyright © 2018 Outbrain. All rights reserved.
+//  Created by oded regev on 28/05/2023.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-//! Project version number for DynamicOutbrainSDK.
-FOUNDATION_EXPORT double DynamicOutbrainSDKVersionNumber;
+//! Project version number for OutbrainSDK.
+FOUNDATION_EXPORT double OutbrainSDKVersionNumber;
 
-//! Project version string for DynamicOutbrainSDK.
-FOUNDATION_EXPORT const unsigned char DynamicOutbrainSDKVersionString[];
+//! Project version string for OutbrainSDK.
+FOUNDATION_EXPORT const unsigned char OutbrainSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <OutbrainSDK/PublicHeader.h>
-
-#import <OutbrainSDK/Outbrain.h>
-#import <OutbrainSDK/OBRequest.h>
-#import <OutbrainSDK/OBPlatformRequest.h>
-#import <OutbrainSDK/OBResponse.h>
-#import <OutbrainSDK/OBRecommendation.h>
-#import <OutbrainSDK/OBRecommendationResponse.h>
-#import <OutbrainSDK/OBLabel.h>
-#import <OutbrainSDK/OBDisclosure.h>
-#import <OutbrainSDK/OBContent.h>
-#import <OutbrainSDK/OBImageInfo.h>
-#import <OutbrainSDK/OBDisclosure.h>
-#import <OutbrainSDK/OBSkAdNetworkData.h>
-#import <OutbrainSDK/OBSettings.h>
-#import <OutbrainSDK/OBResponseRequest.h>
-
-#import <OutbrainSDK/SFCollectionViewCell.h>
-#import <OutbrainSDK/SFTableViewCell.h>
-#import <OutbrainSDK/SmartFeedManager.h>
-
-#import <OutbrainSDK/SFTableViewHeaderCell.h>
-#import <OutbrainSDK/SFTableViewReadMoreCell.h>
-#import <OutbrainSDK/SFCollectionViewHeaderCell.h>
-#import <OutbrainSDK/SFCollectionViewReadMoreCell.h>
-#import <OutbrainSDK/SFHorizontalView.h>
-#import <OutbrainSDK/SFHorizontalTableViewCell.h>
-#import <OutbrainSDK/SFHorizontalCollectionViewCell.h>
-#import <OutbrainSDK/OBVideoWidget.h>
-#import <OutbrainSDK/OBUtils.h>
-#import <OutbrainSDK/SFItemData.h>
-#import <OutbrainSDK/OBViewabilityService.h>
-
-#import <OutbrainSDK/SFWidget.h>
-#import <OutbrainSDK/SFWidgetTableCell.h>
-#import <OutbrainSDK/SFWidgetCollectionCell.h>
-
 
 
